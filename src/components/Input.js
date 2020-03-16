62da9efe4f44a208350225e10f4ba181db368141
Douglas Henrique <doughrqe@gmail.com>
2020-03-16 17:01:21 -0300

    type input

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,9 +3,9 @@ import React from 'react';
 const InputInitial = () => (
   <div>
     <label htmlFor="input-email" data-testid="label-gravatar-email" >Email do gravatar:</label>
-    <input id="input-email" data-testid="input-gravatar-email" />
+    <input type="email" id="input-email" data-testid="input-gravatar-email" />
     <label htmlFor="input-name" data-testid="label-player-name" >Nome do Jogador:</label>
-    <input id="input-Name" data-testid="input-player-name" />
+    <input type="text" id="input-Name" data-testid="input-player-name" />
   </div>
 );
 export default InputInitial;
