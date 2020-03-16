49a5b62d3a0c47183e55d254656703aead5c2cb3
Douglas Henrique <doughrqe@gmail.com>
2020-03-16 15:56:20 -0300

    devtools

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,4 @@ import rootReducer from '../reducers/rootReducer';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer(), composeEnhancers(applyMiddleware(thunk)));
 
-// const store = createStore(rootReducer(), compose(applyMiddleware(thunk)));
-
-
 export default store;
