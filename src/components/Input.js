d0b9537f666c12bbc1c0bb15d38ff54364311f75
Douglas Henrique <doughrqe@gmail.com>
2020-03-16 16:46:05 -0300

    Finalizado componente input com test

---
+++ src/components/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const InputInitial = () => (
+  <div>
+    <label data-testid="label-gravatar-email" >Email do gravatar:</label>
+    <input data-testid="input-gravatar-email" />
+    <label data-testid="label-player-name" >Email do gravatar:</label>
+    <input data-testid="input-player-name" />
+  </div>
+);
+export default InputInitial;
