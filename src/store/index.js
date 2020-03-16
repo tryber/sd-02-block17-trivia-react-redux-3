6250599c8c550f9c81fa6812f616a1f31538512d
Douglas Henrique <doughrqe@gmail.com>
2020-03-16 15:54:49 -0300

    test dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,10 @@ import {
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(rootReducer(), composeEnhancers(applyMiddleware(thunk)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer(), composeEnhancers(applyMiddleware(thunk)));
 
-const store = createStore(rootReducer(), compose(applyMiddleware(thunk)));
+// const store = createStore(rootReducer(), compose(applyMiddleware(thunk)));
 
 
 export default store;
