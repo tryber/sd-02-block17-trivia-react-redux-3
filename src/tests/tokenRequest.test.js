f1d1e52ff5f58278a0686a29b1a0768f1a7b4a32
Mateus Talles <mateustalles@gmail.com>
2020-03-23 14:54:22 -0300

    Obj. destrutcturing on test

--- src/tests/tokenRequest.test.js
+++ src/tests/tokenRequest.test.js
@@ -2,7 +2,11 @@ import tokenRequest from '../services/tokenRequest';
 
 describe('Token is being retrieved successfully', () => {
   test('GET request', async () => {
-    const { response_code: responseCode, response_message: responseMessage, token } = await tokenRequest();
+    const {
+      response_code: responseCode,
+      response_message: responseMessage,
+      token,
+    } = await tokenRequest();
     expect(responseCode).toBe(0);
     expect(responseMessage).toBe('Token Generated Successfully!');
     expect(token).not.toBe(undefined);
