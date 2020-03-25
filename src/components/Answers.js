043d8f01574feb021a284c0e8060d1f0e1459064
Douglas Henrique <doughrqe@gmail.com>
2020-03-25 01:06:23 -0300

    Deixando botoes coloridos de acordo com a resposta

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import './Answers.css';
 
 class Answers extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { results: {} };
+    this.state = { results: {}, array: ['', '', '', ''], };
     this.answers = this.answers.bind(this);
+    this.submitAnswer = this.submitAnswer.bind(this);
+
   }
 
   answers(element) {
@@ -17,9 +19,9 @@ class Answers extends React.Component {
       const aux = (arr.length > 1) ? Math.round((Math.random() * arr.length)) : 0;
       arr2[i] = arr.splice(aux - 1, 1)[0]
     }
-    this.setState({ results: arr2 });
+    this.setState({ results: arr2, array: ['', '', '', ''] });
   }
-  
+
   componentDidUpdate(prevProps) {
     const { question } = this.props;
     if (prevProps.question !== question) {
@@ -27,12 +29,41 @@ class Answers extends React.Component {
     }
   }
 
+  submitAnswer() {
+    const { question: { correct_answer: correct } } = this.props;
+    const { results } = this.state;
+    const index = results.indexOf(correct);
+    this.setState((state) => ({
+      ...state,
+      array: state.array.map((ele, i) => (i === index) ? 'green' : 'red')
+    }));
+  }
 
   render() {
-    const { results } = this.state;
+    const { results, array } = this.state;
+    const { question } = this.props;
+    let correct = '';
+    if (question) {
+      correct = question.correct_answer;
+    }
     return (
       <div>
-        {(Object.keys(results).length > 0) ? results.map((response) => <button key={response}><h3>{response}</h3></button>) : ''}
+        {(Object.keys(results).length > 0) ?
+          results.map((response, index) =>
+            (
+              <button
+                value={response}
+                key={response}
+                data-testid={(response !== correct) ? `wrong-answer-${index}` : "correct-awnser"}
+                className={array[index]}
+                ref={this.response}
+                onClick={this.submitAnswer}
+                disabled={(array[index]) ? true : !true}
+              >
+                <h3>{response}</h3>
+              </button>
+            ))
+          : ''}
       </div>
     );
   }
