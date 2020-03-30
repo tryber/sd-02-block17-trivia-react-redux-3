ed67a3140515b91bd61a4998a8d1c3c80aadb644
douglas-he <doughrqe@gmail.com>
2020-03-30 01:15:10 -0300

    Test Coverage Configuration

---
+++ src/tests/Configuration.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from '../App';
+import rootReducer from '../reducers/rootReducer';
+
+afterEach(cleanup);
+const reducer = {
+  gameReducer: {
+    name: 'aaaaa', gravatarEmail: 'aaaaa', scoreboard: 0, answersClasses: ['', '', '', ''],
+  },
+  categoryReducer: {},
+  getQuestions: { results: [] },
+  timeReducer: { timer: 2 },
+  typeReducer: {
+    types: ['multiple', 'boolean'],
+    Tselected: '',
+  },
+  difficultyreducer: {
+    difficulty: ['easy', 'medium', 'hard'],
+    Dselected: '',
+  },
+};
+
+function renderWithRedux(
+  ui,
+  { store = createStore(rootReducer(), reducer) } = {},
+) {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    store,
+  };
+}
+describe('Test line 9 Configuration', () => {
+  it('test configuration', () => {
+    const history = createMemoryHistory();
+    const { getByText, getByTestId } = renderWithRedux(
+      <Router history={history}>
+        <App />
+      </Router>,
+    );
+    fireEvent.click(getByTestId('config-button'));
+    expect(getByText('Voltar')).toBeInTheDocument();
+    fireEvent.click(getByText('Voltar'));
+    expect(history.location.pathname).toBe('/');
+  });
+});
